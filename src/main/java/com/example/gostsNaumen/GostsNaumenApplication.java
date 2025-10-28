1036dd99d177d8541ff4aa89eb84672358b4233f
Kirill Petrov <kirillpetrov173@gmail.com>
2025-10-28 19:23:56 +0500

    Initial commit

---
+++ src/main/java/com/example/gostsNaumen/GostsNaumenApplication.java
@@ -0,0 +1,13 @@
+package com.example.gostsNaumen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GostsNaumenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GostsNaumenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gostsNaumen/GostsNaumenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gostsNaumen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GostsNaumenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
