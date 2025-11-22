751e9cf1ef80e39a1adf4d72f488d6321d5d8005
Kirill Petrov <kirillpetrov173@gmail.com>
2025-11-22 17:27:06 +0500

    Небольшие правки JavaDoc

--- src/main/java/com/example/gostsNaumen/repository/DocumentRepository.java
+++ src/main/java/com/example/gostsNaumen/repository/DocumentRepository.java
@@ -13,7 +13,13 @@ import java.util.Optional;
  * Репозиторий для работы со стандартами
  */
 @Repository
-public interface DocumentRepository extends JpaRepository<Document, Long>, JpaSpecificationExecutor<Document> {
+public interface DocumentRepository extends JpaRepository<Document, Long> {
+    /**
+     * Поиск пользователя по полному имени записанному в БД
+     *
+     * @param fullName полное имя пользователя
+     * @return сущность Document обернутую в {@link Optional}
+     */
     Optional<Document> findByFullName(String fullName);
 
     Optional<List<Document>> findAllByFullName(String fullName);
