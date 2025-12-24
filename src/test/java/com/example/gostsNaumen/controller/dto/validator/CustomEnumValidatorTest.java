67e50b730c15b6cc1c488c836c95f23e27d78b0f
Андрей <manakin.andrei.66@gmail.com>
2025-12-25 03:40:08 +0500

    Исправил тестовый класс CustomEnumValidatorTest.java

--- src/test/java/com/example/gostsNaumen/controller/dto/validator/CustomEnumValidatorTest.java
+++ src/test/java/com/example/gostsNaumen/controller/dto/validator/CustomEnumValidatorTest.java
@@ -1,10 +1,12 @@
 package com.example.gostsNaumen.controller.dto.validator;
 
+import com.example.gostsNaumen.controller.dto.validator.FakeClasses.FakeCustomEnumValid;
 import com.example.gostsNaumen.entity.model.AdoptionLevelEnum;
 import com.example.gostsNaumen.entity.model.HarmonizationEnum;
 import com.example.gostsNaumen.entity.model.StatusEnum;
 import jakarta.validation.ConstraintValidatorContext;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -16,128 +18,66 @@ import org.mockito.junit.jupiter.MockitoExtension;
  */
 @ExtendWith(MockitoExtension.class)
 public class CustomEnumValidatorTest {
-    /**
-     * Экземпляр тестируемого валидатора
-     */
-    private final CustomEnumValidator validator;
 
     /**
      * Контекст валидации JSR-303 (Bean Validation).
      * Используется для передачи информации о контексте валидации валидатору.
      * Может быть использован для настройки сообщений об ошибках или других параметров.
      */
-    private final ConstraintValidatorContext context;
+    @Mock
+    private ConstraintValidatorContext context;
 
     /**
-     * Поле, необходимое для получения аннотации {@code CustomEnumValid} со значением
-     * {@code enumClass = HarmonizationEnum.class}
+     * Экземпляр тестируемого валидатора.
      */
-    @CustomEnumValid(enumClass = HarmonizationEnum.class)
-    private String harmonizationField;
+    private CustomEnumValidator validator;
 
-    /**
-     * Поле, необходимое для получения аннотации {@code CustomEnumValid} со значением
-     * {@code enumClass = StatusEnum.class}
-     */
-    @CustomEnumValid(enumClass = StatusEnum.class)
-    private String statusField;
-
-    /**
-     * Поле, необходимое для получения аннотации {@code CustomEnumValid} со значением
-     * {@code enumClass = AdoptionLevelEnum.class}
-     */
-    @CustomEnumValid(enumClass = AdoptionLevelEnum.class)
-    private String adoptionLevelField;
-
-    public CustomEnumValidatorTest(
-            @Mock ConstraintValidatorContext context
-    ) {
-        this.validator = new CustomEnumValidator();
-        this.context = context;
+    @BeforeEach
+    void setUp() {
+        validator = new CustomEnumValidator();
     }
 
     /**
-     * Метод, покрывающий корректные вводные данные
+     * Метод, проверяющий успешные случаи валидации
      */
     @Test
-    void shouldAcceptValidEnumValues() throws Exception {
+    void shouldAcceptValidEnumValues() {
 
-        CustomEnumValid annotationHarmonization = this.getClass()
-                .getDeclaredField("harmonizationField")
-                .getAnnotation(CustomEnumValid.class);
+        FakeCustomEnumValid fakeCustomEnumValidHarmonization = new FakeCustomEnumValid(HarmonizationEnum.class);
+        FakeCustomEnumValid fakeCustomEnumValidStatus = new FakeCustomEnumValid(StatusEnum.class);
+        FakeCustomEnumValid fakeCustomEnumValidAdoption = new FakeCustomEnumValid(AdoptionLevelEnum.class);
 
-        CustomEnumValid annotationStatus = this.getClass()
-                .getDeclaredField("statusField")
-                .getAnnotation(CustomEnumValid.class);
+        validator.initialize(fakeCustomEnumValidHarmonization);
 
-        CustomEnumValid annotationAdoption = this.getClass()
-                .getDeclaredField("adoptionLevelField")
-                .getAnnotation(CustomEnumValid.class);
+        Assertions.assertTrue(validator.isValid("Гармонизированный", context));
+        Assertions.assertTrue(validator.isValid("Не гармонизированный", context));
+        Assertions.assertTrue(validator.isValid("Модифицированный", context));
 
-        String[] validEnumValuesHarm = {
-                "Не гармонизированный", "Модифицированный", "Гармонизированный"
-        };
+        validator.initialize(fakeCustomEnumValidStatus);
 
-        String[] validEnumValuesStatus = {
-                "Актуальный", "Отменённый", "Заменённый"
-        };
+        Assertions.assertTrue(validator.isValid("Актуальный", context));
+        Assertions.assertTrue(validator.isValid("Заменённый", context));
+        Assertions.assertTrue(validator.isValid("Отменённый", context));
 
-        String[] validEnumValuesAdoption = {
-                "Национальный", "Межгосударственный", "Отраслевой", "Региональный", "Стандарт Организаций"
-        };
+        validator.initialize(fakeCustomEnumValidAdoption);
 
-        validator.initialize(annotationHarmonization);
-        for (String validEnumValue : validEnumValuesHarm) {
-            Assertions.assertTrue(validator.isValid(validEnumValue, context));
-        }
-
-        validator.initialize(annotationStatus);
-        for (String validEnumValue : validEnumValuesStatus) {
-            Assertions.assertTrue(validator.isValid(validEnumValue, context));
-        }
-
-        validator.initialize(annotationAdoption);
-        for (String validEnumValue : validEnumValuesAdoption) {
-            Assertions.assertTrue(validator.isValid(validEnumValue, context));
-        }
+        Assertions.assertTrue(validator.isValid("Национальный", context));
+        Assertions.assertTrue(validator.isValid("Межгосударственный", context));
+        Assertions.assertTrue(validator.isValid("Отраслевой", context));
+        Assertions.assertTrue(validator.isValid("Региональный", context));
+        Assertions.assertTrue(validator.isValid("Стандарт Организаций", context));
     }
 
     /**
      * Метод, проверяющий некорректные значения
      */
     @Test
-    void shouldNotAcceptInvalidEnumValues() throws Exception {
-        CustomEnumValid annotationHarmonization = this.getClass()
-                .getDeclaredField("harmonizationField")
-                .getAnnotation(CustomEnumValid.class);
+    void shouldNotAcceptInvalidEnumValues() {
+        FakeCustomEnumValid fakeCustomEnumValidHarmonization = new FakeCustomEnumValid(HarmonizationEnum.class);
 
-        String[] validEnumValuesHarm = {
-                "Неправильное значение!!!", "Ещё неправильное", "гармонизированный", "слева ошибка регистра"
-        };
+        validator.initialize(fakeCustomEnumValidHarmonization);
 
-        validator.initialize(annotationHarmonization);
-        for (String validEnumValue : validEnumValuesHarm) {
-            Assertions.assertFalse(validator.isValid(validEnumValue, context));
-        }
-    }
-
-    /**
-     * Метод, проверяющий работу аннотации при передаче неверного класса enum-а
-     */
-    @Test
-    void shouldNotAcceptCorrectValuesBecauseWrongEnumTypeInParam() throws Exception {
-        CustomEnumValid annotationHarmonization = this.getClass()
-                .getDeclaredField("statusField")
-                .getAnnotation(CustomEnumValid.class);
-
-        String[] validEnumValuesHarm = {
-                "Не гармонизированный", "Модифицированный", "Гармонизированный"
-        };
-
-        validator.initialize(annotationHarmonization);
-
-        for (String validEnumValue : validEnumValuesHarm) {
-            Assertions.assertFalse(validator.isValid(validEnumValue, context));
-        }
+        Assertions.assertFalse(validator.isValid("неправильное значение", context));
+        Assertions.assertFalse(validator.isValid("sdfsdf", context));
     }
 }
